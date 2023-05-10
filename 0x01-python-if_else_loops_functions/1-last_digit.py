9dedb4c6d1eaef209e2a7a6869eef1d5163b630d
Pontuagi <benepontuagi@gmail.com>
2023-05-10 02:47:25 +0000

    print last digit 01

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ no_str = str(number)
 no = len(no_str)
 if int(no_str[no - 1]) > 5:
     print(f"Last digit of {number} is {number[no - 1]} and is greater then 5")
-elif int(no_str[no -1]) == 0:
+elif int(no_str[no - 1]) == 0:
     print(f"Last digit of {number} is {number[no - 1]} and is 0")
-elif int(no_str[no -1]) < 6 and int(no_str[no -1]) != 0:
+elif int(no_str[no - 1]) < 6 and int(no_str[no - 1]) != 0:
     print(f"Last digit of {number} is {number[no - 1]} and is less than 6 and not 0")
