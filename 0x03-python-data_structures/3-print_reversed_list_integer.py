c84ed3686cf3e0cbb8f33094780ddad27074b095
Pontuagi <benepontuagi@gmail.com>
2023-05-15 23:40:29 +0000

    tuple with length and 1st characterof a string

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list=[]):
     length = len(my_list)
     while length > 0:
         length -= 1
-        print("{0}".format(my_list[length]))
+        print("{:d}".format(my_list[length]))
