564b3f44cb92d4c583eb83e7391221dae31f8f7c
Pontuagi <benepontuagi@gmail.com>
2023-05-15 22:41:50 +0000

    replace part of a copylist

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     length = len(my_list)
     while length > 0:
-        temp = my_list[length -1]
-        print("{}".format(temp))
         length -= 1
+        print("{0}".format(my_list[length]))
