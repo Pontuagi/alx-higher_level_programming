7025e05c428f36db7ce5d8484e35cdba06485632
Pontuagi <benepontuagi@gmail.com>
2023-05-17 00:58:34 +0000

    print argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    length = len(argv)
+    if length == 2:
+        print("1 argument:")
+    elif length == 1:
+        print("1 argument.")
+    else:
+        print("{} arguments:".format(length - 1))
+    i = 1
+    while length > 1:
+        print("{}: {}".format(i, argv[i]))
+        length -= 1
