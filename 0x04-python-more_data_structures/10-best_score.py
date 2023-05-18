151a756228942841692951fdb12e18f9749f2d83
Pontuagi <benepontuagi@gmail.com>
2023-05-18 03:01:13 +0000

    get max value in dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     max_val = float('-inf')
-    for value in a_dictionary.value():
+    for value in a_dictionary.values():
         if value > max_val:
             max_val = value
     return max_val
