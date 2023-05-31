7b2370c50100f294c145b833c98790d11620814f
Pontuagi <benepontuagi@gmail.com>
2023-05-31 21:12:04 +0000

    print division

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,9 @@ def safe_print_list_integers(my_list=[], x=0):
             if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end='')
                 count += 1
-    except (TypeError, IndexError):
+    except IndexError:
+        raise IndexError("list index out of range")
+    except TypeError:
         pass
     finally:
         print()
