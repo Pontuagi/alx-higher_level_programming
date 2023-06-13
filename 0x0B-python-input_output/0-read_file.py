0ece94ae2eab5a7385e9f31db6e9f8e3a086cd8b
Pontuagi <benepontuagi@gmail.com>
2023-06-14 01:41:33 +0300

    input output. Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+This file contains one function
+the funtion reads a text file(UTF8) and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    function read a text file and print it to stdout
+
+    Args:
+        filename (file): file to open
+
+    """
+    with open(filename, encoding="UTF8") as file_a:
+        for line in file_a:
+            print(line)
