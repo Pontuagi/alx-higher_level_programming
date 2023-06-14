0968f9eba3bd2db622a1b205b83f1219c0f3e5eb
Pontuagi <benepontuagi@gmail.com>
2023-06-14 05:01:34 +0300

    class to JSON

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+'''
+this file contains a script that adds all argumets to a pytho list
+it then saves them to a file
+'''
 import sys
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 try:
