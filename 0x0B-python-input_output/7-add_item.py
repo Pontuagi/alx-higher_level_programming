e3f93c47305e40d30d9d67f024353a764fb622d3
Pontuagi <benepontuagi@gmail.com>
2023-06-14 04:35:38 +0300

    add all arguments to a python list 01

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-# check for existing data
 try:
     existing = load_from_json_file("add_item.json")
 except FileNotFoundError:
     existing = []
-
-# add new arguments to the list
 arguments = sys.argv[1:]
 data = existing + arguments
-
 save_to_json_file(data, "add_item.json")
