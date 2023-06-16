b2058a15012d23197e74f12b35e8d25f6cdc7ef9
Pontuagi <benepontuagi@gmail.com>
2023-06-16 03:05:37 +0300

    class with private attribbute

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+
+"""
+This module contains a class square
+it defines a square
+"""
+
+
 class Square:
     """ A class that defines a square
     
-    Attributes:
+    Args:
         __size (int): The size of the square
     """
     def __init__(self, size):
