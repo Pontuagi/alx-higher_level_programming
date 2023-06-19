dca71f3222d8ee43eba84ffb29237a1c2d7c6a45
Pontuagi <benepontuagi@gmail.com>
2023-06-20 02:10:18 +0300

    json strings

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
 """
 This module contains one class Base
 """
+import json
 
 
 class Base:
@@ -23,3 +24,29 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the Json string represantion of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return ("[]")
+        return (json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON string represantion of list_objs"""
+        if list_objs is None:
+            list_objs = []
+        filename = cls.__name__ + ".json"
+        json_string = cls.to_json_string(
+            [obj.to_dictionary() for obj in list_objs]
+            )
+        with open(filename, 'w') as file:
+            file.write(json_string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the list represented by json_string"""
+        if json_string is None or json_string == '':
+            return ([])
+        return (json.loads(json_string))
