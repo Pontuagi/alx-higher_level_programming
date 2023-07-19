55ade05478edb98223a6c946adea0e207648f6d2
Pontuagi <benepontuagi@gmail.com>
2023-07-19 03:51:38 +0300

    deleting a value in second_table

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -3,4 +3,4 @@ USE hbtn_0c_0;
 
 UPDATE second_table
 SET score = 10
-WHERE = 'Bob';
+WHERE name = 'Bob';
---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,5 @@
+-- Removes all records  with score<=5 in hbtn_0c_0.second_table
+USE hbtn_0c_0;
+
+DELETE FROM second_table
+WHERE score <= 5;
