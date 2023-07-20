c17e7744976f6efa3420a1cae0cc7965f249b12f
Pontuagi <benepontuagi@gmail.com>
2023-07-20 11:48:06 +0300

    4 and 5 correction

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,3 @@
 -- Print the full description of the table first_table
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-AND TABLE_NAME = 'first_table';
+
+SHOW COLUMNS FROM first_table
