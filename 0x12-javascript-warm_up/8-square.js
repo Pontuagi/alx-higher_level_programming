e0298a34e217d44a63d37fdb56205e152ff00248
Pontuagi <benepontuagi@gmail.com>
2023-08-15 04:30:32 +0300

    Print square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+/*
+ * A script that prints a square
+ */
+const { argv } = require('process');
+
+if (!argv[2] || isNaN(argv[2])) {
+  console.log('Missing size');
+} else {
+  const numberOfX = Number(argv[2]);
+  for (let i = 0; i < numberOfX; i++) {
+    console.log('x'.repeat(numberOfX));
+  }
+}
