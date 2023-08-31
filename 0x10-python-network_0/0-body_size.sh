82b4c966a9199d58f1c8f9e469f675ea7eaf7adf
Pontuagi <benepontuagi@gmail.com>
2023-08-31 23:55:47 +0300

    curl to the end

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#/bin/bash
+#!/bin/bash
 # Accepts a URL, sends a request to it and display size of the body of response
 curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
-#/bin/bash
+#!/bin/bash
 # Sends a DELETE request to the URL passed and first argument and display body of responce
 curl -s -X DELETE "$1"
