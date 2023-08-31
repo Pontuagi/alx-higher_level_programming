8d4ef5d1b67e57230619c499d905255b9f5d6a2c
Pontuagi <benepontuagi@gmail.com>
2023-08-31 14:19:16 -0700

    Curl Method

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Accepts a URL, sends a GET request to it and idsplays the body of the responce
-curl -s -o /dev/null -w "%{http-code}" "$1" || [ "$(cat)" = "200" ] && curl -s "$1"
+curl -sL "$1"
