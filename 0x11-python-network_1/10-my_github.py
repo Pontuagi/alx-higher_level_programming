43bd5e4f62873fd72cc6758bf82b4ea43fe3d842
Pontuagi <benepontuagi@gmail.com>
2023-09-02 15:25:35 -0700

    my github

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+A script that takes Github credentials
+Uses github api to display your id
+"""
+
+import requests
+import sys
+
+
+def get_github_user_id(username, access_token):
+    """ A function to fetch github id """
+    url = f"https://api.github.com/user"
+    headers = {
+        "Authorization": f"Basic {username}:{access_token}"
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            user_data = response.json()
+            user_id = user_data["id"]
+            print(f"{user_id}")
+        else:
+            print("None")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    access_token = sys.argv[2]
+    get_github_user_id(username, access_token)
