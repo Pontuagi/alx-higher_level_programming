5134f42b04906db92024e23fa564983be447fa81
Pontuagi <benepontuagi@gmail.com>
2023-09-02 13:33:47 -0700

    urls status correction

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,26 +7,13 @@ It fetches a url
 
 import urllib.request
 
-
-def fetch_status():
-    """
-    Function to fetch a url
-    Uses the package urllib
-    """
-    url = "https://alx-intranet.hbtn.io/status"
-    try:
-        with urllib.request.urlopen(url) as response:
-            body_bytes = response.read()
-            body_str = body_bytes.decode("utf-8")
-
-            print("Body response:")
-            print("    - type:", type(body_bytes))
-            print("    - content:", repr(body_bytes))
-            print("    - utf8 content:", body_str)
-
-    except urllib.error.URLError as e:
-        print("Error:", e)
-
-
 if __name__ == "__main__":
-    fetch_status()
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        body_bytes = response.read()
+        body_str = body_bytes.decode("utf-8")
+
+        print("Body response:")
+        print("    - type:", type(body_bytes))
+        print("    - content:", repr(body_bytes))
+        print("    - utf8 content:", body_str)
