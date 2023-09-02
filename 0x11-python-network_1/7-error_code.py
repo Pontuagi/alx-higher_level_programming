2a19ac834994d227f140ff2e9ef12b0679a87472
Pontuagi <benepontuagi@gmail.com>
2023-09-02 14:57:18 -0700

    Error code correction

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -14,10 +14,7 @@ if __name__ == "__main__":
     response_code = response.status_code
     response_body = response.text
 
-    print("Body response:")
-    print("    - type:", type(response_body))
-    print("    - content:", repr(response_body))
-    print("    - utf8 content:", response_body)
+    print(repr(response_body))
 
     if response_code >= 400:
         print(f"Error code: {response_code}")
