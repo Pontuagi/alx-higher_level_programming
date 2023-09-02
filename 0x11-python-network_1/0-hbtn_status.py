83e024c1804debf71a76cb0ed64d843093a72bf6
Pontuagi <benepontuagi@gmail.com>
2023-09-02 21:35:28 +0300

    Status of url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,17 +8,25 @@ It fetches a url
 import urllib.request
 
 
-url = "https://alx-intranet.hbtn.io/status"
+def fetch_status(url):
+    """
+    Function to fetch a url
+    Uses the package urllib
+    """
+    try:
+        with urllib.request.urlopen(url) as response:
+            body_bytes = response.read()
+            body_str = body_bytes.decode("utf-8")
 
-try:
-    with urllib.request.urlopen(url) as response:
-        body_bytes = response.read()
-        body_str = body_bytes.decode("utf-8")
+            print("Body response:")
+            print("    - type:", type(body_bytes))
+            print("    - content:", repr(body_bytes))
+            print("    - utf8 content:", body_str)
 
-        print("Body response:")
-        print("    - type:", type(body_bytes))
-        print("    - content:", repr(body_bytes))
-        print("    - utf8 content:", body_str)
+    except urllib.error.URLError as e:
+        print("Error:", e)
 
-except urllib.error.URLError as e:
-    print("Error:", e)
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    fetch_status(url)
