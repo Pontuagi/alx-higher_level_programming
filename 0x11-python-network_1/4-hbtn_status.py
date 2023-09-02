4655b97536e6ae2cf9f1edc15eb07b22146a4af6
Pontuagi <benepontuagi@gmail.com>
2023-09-02 14:06:03 -0700

    status of url correction

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -10,13 +10,9 @@ import requests
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
 
-    try:
-        response = requests.get(url)
-        response_type = type(response.text).__name__
-        response_content = response.text
-        print("Body response:")
-        print("    - type:", type(response_type))
-        print("    - content:", repr(response_content))
-
-    except requests.exceptions.RequestException as e:
-        print("Error:", e)
+    response = requests.get(url)
+    response_type = type(response.text)
+    response_content = response.text
+    print("Body response:")
+    print("    - type:", type(response_type))
+    print("    - content:", repr(response_content))
