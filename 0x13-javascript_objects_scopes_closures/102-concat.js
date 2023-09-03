7a9b8e7d7572952061faaa29ff0e985e26da5374
Pontuagi <benepontuagi@gmail.com>
2023-09-03 19:00:19 +0300

    Concat file

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,45 @@
+#!/usr/bin/node
+
+/*
+ * a script that concats 2 files.
+ */
+
+const fs = require('fs');
+const path = require('path');
+
+if (process.argv.length !== 5) {
+  console.error('Usage: ./102-concat <file_1> <file_2> <dest_file>');
+  process.exit(1);
+}
+
+const sourceFile1 = process.argv[2];
+const sourceFile2 = process.argv[3];
+const destinationFile = process.argv[4];
+
+// Read the content of the first source file
+fs.readFile(sourceFile1, 'utf8', (err, data1) => {
+  if (err) {
+    console.error(`Error reading ${sourceFile1}: ${err.message}`);
+    process.exit(1);
+  }
+
+  // Read the content of the second source file
+  fs.readFile(sourceFile2, 'utf8', (err, data2) => {
+    if (err) {
+      console.error(`Error reading ${sourceFile2}: ${err.message}`);
+      process.exit(1);
+    }
+
+    // Concatenate the contents of both files
+    const concatenatedContent = data1 + data2;
+
+    // Write the concatenated content to the destination file
+    fs.writeFile(destinationFile, concatenatedContent, (err) => {
+      if (err) {
+        console.error(`Error writing to ${destinationFile}: ${err.message}`);
+        process.exit(1);
+      }
+
+    });
+  });
+});
