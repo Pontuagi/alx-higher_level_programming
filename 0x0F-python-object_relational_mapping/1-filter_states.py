8b7e535b4d727ac2dc139f071f160d884b5ceffa
Pontuagi <benepontuagi@gmail.com>
2023-09-05 11:21:02 +0300

    filter state 01

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,9 +11,6 @@ import MySQLdb
 if __name__ == '__main__':
     import sys
 
-    if len(sys.argv) != 4:
-        return
-
     # Connect to MySQL server
     db = MySQLdb.connect(
         host='localhost',
