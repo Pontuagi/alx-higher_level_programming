bbe346f9ba5d25b93b0fbf120cdf348d118e84ce
Pontuagi <benepontuagi@gmail.com>
2023-09-27 01:36:47 +0300

    print number of movies

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+/*
+ * A script that prints the number of movies where the character
+ * 'Wedge Antilles' is present
+ */
+const request = require('request');
+
+if (process.argv.length !== 3) {
+  console.error('Usage: ./4-starwars_count.js <URL>');
+  process.exit(1);
+}
+
+const movieUrl = process.argv[2];
+
+request.get(movieUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error making Get Request:', error);
+  } else if (response.statusCode !== 200) {
+    console.error(`Requet failed with ${response.statusCode} code`);
+  } else {
+    try {
+      const filmsData = JSON.parse(body);
+      const antillesMovies = filmsData.results.filter((film) => {
+        return film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/');
+      });
+      console.log(`${antillesMovies.length}`);
+    } catch (parseError) {
+      console.error('Error parsing API response:', parseError);
+    }
+  }
+});
