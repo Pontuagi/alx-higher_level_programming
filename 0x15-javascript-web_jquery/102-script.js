cf2e6cc3efcd4e1b7cd992084fee275f575b65ba
Pontuagi <benepontuagi@gmail.com>
2023-10-05 00:04:21 +0300

    Press enter

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,34 @@
+/*
+ * JavaScript script that fetches and prints how to say “Hello” depending
+ * on the language
+ */
+
+// Wait for the document to be fully loaded
+$(document).ready(function () {
+  // Add a click event handler to INPUT#btn_translate
+  $('#btn_translate').click(function () {
+    // Get the language code from INPUT#language_code
+    const languageCode = $('#language_code').val();
+
+    // Make an AJAX GET request to fetch the translation
+    $.ajax({
+      url: 'https://www.fourtonfish.com/hellosalut/hello/',
+      method: 'POST',
+      dataType: 'json',
+      data: {
+        lang: languageCode,
+      },
+      success: function (data) {
+        // Extract the translation from the response
+        const translation = data.hello;
+
+        // Display the translation in the HTML tag DIV#hello
+        $('#hello').text(translation);
+      },
+      error: function () {
+        // Handle errors if the request fails
+        console.error('Failed to fetch translation data.');
+      },
+    });
+  });
+});
